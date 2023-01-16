f2df18401f3e4739d8b0b24564570fa04e247a56
Shegda Igor <igorbeef@gmail.com>
2023-01-16 20:31:00 +0200

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import HomePage from 'pages/HomePage';
+import MovieDetails from 'pages/MovieDetails';
+import Movies from 'pages/Movies';
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header>
+        <nav>
+          <NavLink to="/" className="NavLink">
+            Home
+          </NavLink>
+          <NavLink to="/movies" className="NavLink">
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:moviesId" element={<MovieDetails />} />
+          <Route path="cast" />
+          <Route path="reviews" />
+        </Routes>
+      </div>
     </div>
   );
 };
